106272d7b1fc3a8bd9310be4a46ad8bbc620ac63
Shangxin <shangxin@outlook.com>
2016-11-02 15:32:04 +0800

    Revert "修改发布任务"
    This reverts commit 9fa06f35e9d45ae0d51b6c3c9614f1777cde72f6.

--- PublishTask.php
+++ PublishTask.php
@@ -6,17 +6,6 @@
  * Time: 2:23
  */
 
-if( !isset( $_POST ) || !isset($_POST['title']) ){
-    echo json_encode(array("success" => "0","error" => "6","message" => "task information is wrong."));
-    exit();
-}
-$title = $_POST['title'];
-$text = $_POST['text'];
-$price = $_POST['price'];
-$need_count = $_POST['need_count'];
-$session_id = $_POST['session_id'];
-
-session_id($session_id);
 session_start();
 if( !isset( $_SESSION['username'] ) ){
     echo json_encode(array("success" => "0","error" => "5","message" => "cannot found SESSION[username]."));
@@ -27,6 +16,15 @@ include 'User.php';
 
 $user = new User($_SESSION['username']);
 $user->updateUserInfo($conn);
+
+if( !isset( $_POST ) || !isset($_POST['title']) ){
+    echo json_encode(array("success" => "0","error" => "6","message" => "task information is wrong."));
+    exit();
+}
+$title = $_POST['title'];
+$text = $_POST['text'];
+$price = $_POST['price'];
+$need_count = $_POST['need_count'];
 
 if ( $user->publishTask($conn ,$title ,$text ,$price ,$need_count) )
     echo json_encode(array("success" => "1","error" => "0","message" => "task publish successful."));
